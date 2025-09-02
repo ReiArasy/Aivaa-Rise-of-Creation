b31ae3736553a317ebc403cb7188ebb309ec8e53
Rifqi Reissal Arasy <rifqiarasy@gmail.com>
2025-09-02 21:53:24 +0700

    Aivaa: Rise of Creation

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -7,5 +7,5 @@ func _process(delta: float) -> void:
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
-	# Langsung geser posisi node
+	# move position node
 	position += direction.normalized() * move_speed * delta
